433d96721b32d725a91cdc0ca6a1e6e8f981bf43
GirishMadheswaran <girishgv997@gmail.com>
2022-05-31 19:09:14 +0530

    initial commit

--- lib/hello-app-stack.ts
+++ lib/hello-app-stack.ts
@@ -1,16 +1,53 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack, StackProps, RemovalPolicy } from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as api from 'aws-cdk-lib/aws-apigateway';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import { ApiGatewayToLambda, ApiGatewayToLambdaProps } from '@aws-solutions-constructs/aws-apigateway-lambda';
+import { LambdaToDynamoDB, LambdaToDynamoDBProps } from '@aws-solutions-constructs/aws-lambda-dynamodb';
 
 export class HelloAppStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'HelloAppQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const helloFunc = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'hello.handler'
+    });
+
+    const lambda_ddb_props: LambdaToDynamoDBProps = {
+      lambdaFunctionProps: {
+        code: lambda.Code.fromAsset(`lambda`),
+        runtime: lambda.Runtime.NODEJS_12_X,
+        handler: 'hitcounter.handler',
+        environment: {
+          DOWNSTREAM_FUNCTION_NAME: helloFunc.functionName
+        }
+      },
+      dynamoTableProps: {
+        tableName: 'SolutionsConstructsHits',
+        partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+        removalPolicy: RemovalPolicy.DESTROY
+      }
+    };
+
+    const hitcounter = new LambdaToDynamoDB(this, 'LambdaToDynamoDB', lambda_ddb_props);
+
+    
+    helloFunc.grantInvoke(hitcounter.lambdaFunction);
+
+    const api_lambda_props: ApiGatewayToLambdaProps = {
+      existingLambdaObj: hitcounter.lambdaFunction,
+      apiGatewayProps: {
+        defaultMethodOptions: {
+          authorizationType: api.AuthorizationType.IAM
+        }
+      }
+    };
+
+    new ApiGatewayToLambda(this, 'ApiGatewayToLambda', api_lambda_props);
   }
 }
